4638e343af65508f06b66935e057d942bfb002c8
Rishabh Jain <jainrishabh71011@gmail.com>
2022-05-19 19:05:09 +0530

    ..

--- frontend-app/src/container/About/About.jsx
+++ frontend-app/src/container/About/About.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
-import './About.scss'
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+
+import { AppWrap, MotionWrap } from '../../wrapper';
+import './About.scss';
+import { urlFor, client } from '../../client';
+
+
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      
+    </div>
   )
 }
 
